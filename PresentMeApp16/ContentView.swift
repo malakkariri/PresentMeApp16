72edb00bc93c0a7838592cd85f0ac9c4a3bbefc9
malakkariri <malakkariri@gmail.com>
2023-10-08 11:35:07 +0300

    Initial Commit

---
+++ PresentMeApp16/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PresentMeApp16
+//
+//  Created by malak kariri on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PresentMeApp16/PresentMeApp16App.swift
@@ -0,0 +1,17 @@
+//
+//  PresentMeApp16App.swift
+//  PresentMeApp16
+//
+//  Created by malak kariri on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct PresentMeApp16App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
